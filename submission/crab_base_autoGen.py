10e5f0fe2ea0ebcd313d38c62506c403756ee454
Jan-Frederik <jschulte@cern.ch>
2017-05-03 20:26:27 +0200

    additional mass points for expected limits

--- submission/crab_base_autoGen.py
+++ submission/crab_base_autoGen.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'limits_forDM_4500_20170430_175135'
+config.General.requestName = 'limits_forDM_3375_20170503_023706'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 
@@ -12,18 +12,18 @@ config.section_("JobType")
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = 'dummyPSet.py'
 config.JobType.scriptExe= 'runLimits'
-config.JobType.scriptArgs= ['dummy=dummy.py','tarFile=gridPack.tar','outputTag=forDM','mass=4500','nIter=500000','nToys=50','expected=1','config=ICHEPDimuon_width350']
+config.JobType.scriptArgs= ['dummy=dummy.py','tarFile=gridPack.tar','outputTag=forDM','mass=3375','nIter=50000','nToys=10','expected=1','config=ICHEPDielectron_width350']
 config.JobType.inputFiles= ['gridPack.tar',os.environ['CMSSW_BASE']+'/bin/'+os.environ['SCRAM_ARCH']+'/combine','FrameworkJobReport.xml']
-config.JobType.outputFiles= ['expectedLimit_ICHEPDimuon_width350_forDM_4500.root']
+config.JobType.outputFiles= ['expectedLimit_ICHEPDielectron_width350_forDM_3375.root']
 
 
 config.section_("Data")
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 1
-config.Data.totalUnits = 20
+config.Data.totalUnits = 100
 config.Data.outputPrimaryDataset = 'forDM'
 config.Data.outputDatasetTag = 'test'
-config.Data.outLFNDirBase = '/store/user/jschulte/limits/ICHEPDimuon_width350'
+config.Data.outLFNDirBase = '/store/user/jschulte/limits/ICHEPDielectron_width350'
  
 config.section_("Site")
 config.Site.storageSite = "T2_DE_RWTH"
